a244b2e9b3eed1c7c9370066b53f6e7c8cc1b9d2
CoolingTool <CoolingTool@users.noreply.github.com>
2022-11-19 18:42:20 -0600

    Kankitty turn accelation
    except i dont rally think this is accelation but who cares

--- src/Enemies/Kankitty.gd
+++ src/Enemies/Kankitty.gd
@@ -6,24 +6,29 @@ export var gravity:float = 20
 export var maxfallspeed:float = 340
 export var numsteps:int = 20
 export var speed:float = 25
+export var offstepspeed:float = .2
+export var turnprecision:float = .1e-2
 
 var velocity := Vector2()
+var targetxspeed:float = 0
 var steps:int = 0
 
 onready var sprite := $AnimatedSprite
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	# strokin...
 	velocity.y = min(velocity.y + gravity, maxfallspeed)
+	
+	velocity.x = lerp(velocity.x, targetxspeed, 1 - pow(turnprecision, delta))
 	
 	velocity = move_and_slide(velocity, UP)
 
 func _on_AnimatedSprite_frame_changed():
+	if steps % 2 == 0:
+		targetxspeed = speed * -sign(sprite.scale.x)
+	else:
+		targetxspeed *= offstepspeed
 	if steps > numsteps:
 		steps = 0
 		sprite.scale.x *= -1
-	if steps % 2 == 0:
-		velocity.x = speed * -sign(sprite.scale.x)
-	else:
-		velocity.x *= .4
 	steps += 1
