8bd23752b0201f2f508b1b6c237ae0588ca4487f
CoolingTool <CoolingTool@users.noreply.github.com>
2022-11-19 21:01:59 -0600

    Kankitties now turn at walls
    basically just so i could learn how raycasts work (they are pretty easy)

--- src/Enemies/Kankitty.gd
+++ src/Enemies/Kankitty.gd
@@ -4,16 +4,17 @@ const UP := Vector2(0, -1)
 
 export var gravity:float = 20
 export var maxfallspeed:float = 340
-export var numsteps:int = 20
-export var speed:float = 25
-export var offstepspeed:float = .2
-export var turnprecision:float = .1e-2
+export var numsteps:int = 31
+export var speed:float = 30
+export var offstepspeed:float = 0.005
+export var turnprecision:float = .5e-2
 
 var velocity := Vector2()
 var targetxspeed:float = 0
-var steps:int = 0
+var steps:int = ceil(float(numsteps) / 2)
 
-onready var sprite := $AnimatedSprite
+onready var collider := $CollisionShape2D
+onready var wallcast := $CollisionShape2D/Wallcast
 
 func _physics_process(delta):
 	# strokin...
@@ -23,12 +24,18 @@ func _physics_process(delta):
 	
 	velocity = move_and_slide(velocity, UP)
 
-func _on_AnimatedSprite_frame_changed():
-	if steps % 2 == 0:
-		targetxspeed = speed * -sign(sprite.scale.x)
-	else:
+func calc_xspeed():
+	targetxspeed = speed * -sign(collider.scale.x)
+	if steps % 2 != 0:
 		targetxspeed *= offstepspeed
-	if steps > numsteps:
+
+func _on_AnimatedSprite_frame_changed():
+	calc_xspeed()
+	var stepturn = steps > numsteps
+	var mustturn = !stepturn and wallcast.is_colliding()
+	if stepturn or mustturn:
 		steps = 0
-		sprite.scale.x *= -1
+		collider.scale.x *= -1
+		if mustturn:
+			calc_xspeed()
 	steps += 1
