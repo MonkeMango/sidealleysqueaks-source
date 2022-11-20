81af2b9e72cef0ca89b2e5c21c861302454a3ba0
CoolingTool <CoolingTool@users.noreply.github.com>
2022-11-20 08:37:48 -0600

    🙄🙄🙄

--- src/Enemies/Kankitty.gd
+++ src/Enemies/Kankitty.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 const UP := Vector2(0, -1)
 
-export var gravity:float = 20
+export var gravity:float = 10
 export var maxfallspeed:float = 340
 export var numsteps:int = 16
 export var speed:float = 20
@@ -25,21 +25,19 @@ func _ready():
 func _physics_process(delta):
 	# strokin...
 	velocity.y = min(velocity.y + gravity, maxfallspeed)
-	
 	velocity.x = lerp(velocity.x, targetxspeed, 1 - pow(turnprecision, delta))
-	
 	velocity = move_and_slide(velocity, UP)
 
 func _frame_changed():
 	if ready and sprite.animation == 'walk':
-		if steps > numsteps * 2 or smart_turn():
+		if numsteps >= 0 and steps > numsteps * 2 or smart_turn():
 			steps = 0
 			collider.scale.x *= -1
 			if !startedwalk:
 				sprite.play('turn')
 		else:
 			startedwalk = false
-		steps += 1
+		steps += 1 
 		targetxspeed = speed * -sign(collider.scale.x)
 
 func _animation_finished():
