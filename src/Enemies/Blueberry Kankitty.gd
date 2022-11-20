8f457b2e12a5a2aba1232928b765a31873182830
CoolingTool <CoolingTool@users.noreply.github.com>
2022-11-20 03:59:54 -0600

    turn

--- src/Enemies/Blueberry Kankitty.gd
+++ src/Enemies/Blueberry Kankitty.gd
@@ -3,4 +3,4 @@ extends "res://src/Enemies/Kankitty.gd"
 onready var floorcast := $CollisionShape2D/Floorcast
 
 func smart_turn() -> bool:
-	return .smart_turn() or !floorcast.is_colliding()
+	return .smart_turn() or (is_on_floor() and !floorcast.is_colliding())
--- src/Enemies/Kankitty.gd
+++ src/Enemies/Kankitty.gd
@@ -4,16 +4,17 @@ const UP := Vector2(0, -1)
 
 export var gravity:float = 20
 export var maxfallspeed:float = 340
-export var numsteps:int = 31
-export var speed:float = 30
-export var offstepspeed:float = 0.005
+export var numsteps:int = 16
+export var speed:float = 20
 export var turnprecision:float = .5e-2
 
 var velocity := Vector2()
 var targetxspeed:float = 0
-var steps:int = ceil(float(numsteps) / 2)
+var steps:int = numsteps
+onready var ready:bool = true
 
 onready var collider := $CollisionShape2D
+onready var sprite := $CollisionShape2D/AnimatedSprite
 onready var wallcast := $CollisionShape2D/Wallcast
 
 func _ready():
@@ -27,21 +28,19 @@ func _physics_process(delta):
 	
 	velocity = move_and_slide(velocity, UP)
 
-func calc_xspeed():
-	targetxspeed = speed * -sign(collider.scale.x)
-	if steps % 2 != 0:
-		targetxspeed *= offstepspeed
+func _frame_changed():
+	if ready and sprite.animation == 'walk':
+		if steps > numsteps * 2 or smart_turn():
+			steps = 0
+			collider.scale.x *= -1
+			sprite.play('turn')
+		steps += 1
+		targetxspeed = speed * -sign(collider.scale.x)
 
+func _animation_finished():
+	if sprite.animation != 'walk':
+		sprite.play('walk')
+
+# for blueberry kankitty
 func smart_turn() -> bool:
 	return wallcast.is_colliding()
-
-func _on_AnimatedSprite_frame_changed():
-	calc_xspeed()
-	var turn = steps > numsteps
-	var instant_turn = !turn and smart_turn()
-	if turn or instant_turn:
-		steps = 0
-		collider.scale.x *= -1
-		if instant_turn:
-			calc_xspeed()
-	steps += 1
