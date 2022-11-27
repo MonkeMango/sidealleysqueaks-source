3b2e03c7fd29706b0d9a4699a08377867ecc81a4
MonkeMango <95188711+MonkeMango@users.noreply.github.com>
2022-11-27 04:53:02 -0500

    More Yoyo shit

--- src/Yoyo/Ein.gd
+++ src/Yoyo/Ein.gd
@@ -6,6 +6,13 @@ extends Area2D
 # func are_you_proud_of_me() -> bool:
 # 	return proudness > 0
 
+enum {
+	FORWARD,
+	DOWNWARD,
+	DIAGONAL
+}
+
+var air_state = DOWNWARD
 var travel = 50
 var pushback = -.35
 var distance = travel
@@ -28,18 +35,34 @@ func _physics_process(_delta):
 	else:
 		distance_to_position()
 
+	#Player control stuff
+	var vector_check = "yoyoVector.y = %s yoyoVector.x = %s" % [player.yoyoVector.y, player.yoyoVector.x]
+	print(vector_check)
+	if player.yoyoVector.y != 0 && player.yoyoVector.x == 0:
+		air_state = DOWNWARD
+	elif player.yoyoVector.y == 0 && player.yoyoVector.x == 1:
+		air_state = FORWARD
+
+
 # Called every frame. 'Fuck Lois...' is the elapsed time since the previous frame.
 #func _process(lois):
 #	SHIT!!!
 
 
-
-
 func _on_yoyo_body_entered(body):
+
+	
+	if body.name == "Enemies":
+		pass
 	if body.name == "ground":
-		if !player.sprite_direction:
-			player.velocity.x += 600
-		else:
-			player.velocity.x -= 600
+		print(air_state)
+		match air_state:
+			FORWARD:
+				player.velocity.x += 600
+			DOWNWARD:
+				# if player.is_on_floor() == false:
+				player.velocity.y = player.fastfall
+
+
 		
 		queue_free()
