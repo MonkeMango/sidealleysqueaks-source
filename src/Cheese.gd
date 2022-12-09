57f9ccf2c5af6d6df86843ada5b5bcdb92b35b01
CoolingTool <CoolingTool@users.noreply.github.com>
2022-12-08 18:11:34 -0600

    Collectable objects and collectable hud

---
+++ src/Cheese.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+export var varname:String = 'cheese'
+
+onready var start := Vector2(position.x, position.y)
+
+func _process(_delta):
+	position.y = start.y + sin(OS.get_ticks_msec() * .004) * 1.2
+
+func _on_body_entered(body):
+	if body.get_collision_layer_bit(15):
+		var current = body.get(varname)
+		if current != null:
+			body.set(varname, current + 1)
+			# TODO: ad cool particle
+			queue_free()
